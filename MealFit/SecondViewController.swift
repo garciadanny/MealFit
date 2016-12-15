cddbe2566b106211f1e2d6fb65068085280f5690
Danny Garcia <dannygarcia.me@gmail.com>
2016-12-15 17:23:09 -0500

    Initial Commit

---
+++ MealFit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MealFit
+//
+//  Created by Danny Garcia on 12/15/16.
+//  Copyright © 2016 Danny Garcia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
